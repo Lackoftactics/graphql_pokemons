178109de2a82fbfad062eccd1a0fddf8d4270377
Przemek Mroczek <przemyslawmroczek@o2.pl>
2016-10-18 03:10:25 +0200

    Clean graphql_controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,14 +1,10 @@
 class GraphqlController < ApplicationController
   protect_from_forgery  with: :null_session
 
-  def index
-  end
-
   def create
     schema = GraphQL::Schema.new(query: QueryType, mutation: MutationRoot)
     result = schema.execute(params[:query])
 
     render json: result
   end
-
 end
